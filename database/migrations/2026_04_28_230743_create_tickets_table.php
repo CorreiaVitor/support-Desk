cead4275f0f2fbc9c540c66cdea0a471e0b48b44
Vitor Correia <vitorcorreia.git@gmail.com>
2026-04-28 21:21:21 -0300

    feat: create initial help desk database structure

---
+++ database/migrations/2026_04_28_230743_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->foreignId('requester_id')->constrained('users')->onDelete('cascade')->restrictOnDelete();
+            $table->foreignId('assigned_to_id')->nullable()->constrained('users')->nullOnDelete();
+            $table->string('status')->default('open');
+            $table->string('priority')->default('medium');
+            $table->timestamp('resolved_at')->nullable();
+            $table->timestamp('closed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tickets');
+    }
+};
