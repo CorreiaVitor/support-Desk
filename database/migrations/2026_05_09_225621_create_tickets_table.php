ddf4be4dc9bf19c89ba2bf0fa9a9ca9445c3562d
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-09 20:00:46 -0300

    feat: create tickets table

---
+++ database/migrations/2026_05_09_225621_create_tickets_table.php
@@ -0,0 +1,52 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('number')->unique();
+            $table->string('title', 255);
+            $table->text('description')->nullable();
+
+            $table->foreignId('requester_id')
+                ->constrained('users')
+                ->onDelete('restrict');
+
+            $table->foreignId('technican_id')
+                ->nullable()
+                ->constrained('users')
+                ->onDelete('restrict');
+
+            $table->foreignId('category_id')
+                ->constrained()
+                ->onDelete('restrict');
+
+            $table->foreignid('priority_id')
+                ->constrained()
+                ->onDelete('restrict');
+
+            $table->foreignId('ticket_status_id')
+                ->constrained()
+                ->onDelete('restrict');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('ticket');
+    }
+};
