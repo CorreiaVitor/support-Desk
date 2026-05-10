8bd290f53285c34df3ac40dea54833cd15427e9e
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-10 08:58:29 -0300

    feat: add Ticket  model

---
+++ app/Models/Ticket.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+#[Fillable([
+    'number',
+    'title',
+    'description',
+    'requester_id',
+    'technican_id',
+    'category_id',
+    'priority_id',
+    'ticket_status_id'
+])]
+
+class Ticket extends Model
+{
+    protected function casts(): array
+    {
+        return [
+            'number' => 'integer',
+        ];
+    }
+
+    public function requester(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'requester_id');
+    }
+
+    public function technician(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'technician_id');
+    }
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function priority(): BelongsTo
+    {
+        return $this->belongsTo(Priority::class);
+    }
+
+    public function ticketStatus(): BelongsTo
+    {
+        return $this->belongsTo(Ticket::class);
+    }
+
+    public function comments(): HasMany
+    {
+        return $this->hasMany(TicketComment::class);
+    }
+}
