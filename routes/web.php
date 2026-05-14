c8a5d05a8ff39e8c5c8f5d1bb09918c510b8649d
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-14 19:25:40 -0300

    chore: add temporary supportdesk view routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'dashboard.index')->name('dashboard.index');
+
+Route::view('/tickets', 'tickets.index')->name('tickets.index');
+Route::view('/tickets/create', 'tickets.create')->name('tickets.create');
+Route::view('/tickets/1001', 'tickets.show')->name('tickets.show');
+
+Route::view('/admin/users', 'admin.users.index')->name('admin.users.index');
+Route::view('/admin/users/create', 'admin.users.create')->name('admin.users.create');
+Route::view('/admin/users/1/edit', 'admin.users.edit')->name('admin.users.edit');
+
+Route::view('/admin/profiles', 'admin.profiles.index')->name('admin.profiles.index');
+Route::view('/admin/profiles/create', 'admin.profiles.create')->name('admin.profiles.create');
+Route::view('/admin/profiles/1/edit', 'admin.profiles.edit')->name('admin.profiles.edit');
+
+Route::view('/admin/categories', 'admin.categories.index')->name('admin.categories.index');
+Route::view('/admin/categories/create', 'admin.categories.create')->name('admin.categories.create');
+Route::view('/admin/categories/1/edit', 'admin.categories.edit')->name('admin.categories.edit');
+
+Route::view('/admin/priorities', 'admin.priorities.index')->name('admin.priorities.index');
+Route::view('/admin/statuses', 'admin.statuses.index')->name('admin.statuses.index');
+
+Route::view('/technician/tickets', 'technician.tickets.index')->name('technician.tickets.index');
+Route::view('/user/tickets', 'user.tickets.index')->name('user.tickets.index');
+Route::view('/supervisor/dashboard', 'supervisor.dashboard')->name('supervisor.dashboard');
