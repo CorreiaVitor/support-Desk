1429a7b45d3bd0913e639dfb7ac59f2050277c91
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-18 22:13:28 -0300

    feat: add tickets index controller

---
+++ app/Http/Controllers/TicketController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ticket;
+use Illuminate\Http\Request;
+
+class TicketController extends Controller
+{
+    public function index()
+    {
+
+        $tickets = Ticket::with([
+            'requester',
+            'technician',
+            'category',
+            'priority',
+            'ticketStatus',
+            'comments'
+        ])
+            ->latest()
+            ->get();
+
+        return view('tickets.index', ['tickets' => $tickets]);
+    }
+}
