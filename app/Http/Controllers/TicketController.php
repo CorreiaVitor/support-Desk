3f7b0e99af8f25dfadddb31deaf3d36d3c8dc374
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-21 21:32:33 -0300

    feat: add ticket create and store actions

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTicketRequest;
+use App\Models\Category;
+use App\Models\Priority;
 use App\Models\Ticket;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\View\View;
 
 class TicketController extends Controller
 {
-    public function index()
+    public function index(): View
     {
 
         $tickets = Ticket::with([
@@ -24,17 +29,30 @@ class TicketController extends Controller
         return view('tickets.index', ['tickets' => $tickets]);
     }
 
-    public function show(Ticket $ticket)
+    public function create(): View
+    {
+        $categories = Category::where('is_active', true)->get();
+        $priorities = Priority::where('is_active', true)->get();
+
+        return view('tickets.create', compact('categories', 'priorities'));
+    }
+
+    public function store(StoreTicketRequest $request)
+    {
+        return to_route('tickets.index');
+    }
+
+    public function show(Ticket $ticket): View
     {
 
         $ticket->load([
-                'requester',
-                'technician',
-                'category',
-                'priority',
-                'ticketStatus',
-                'comments' => fn ($query) => $query->with('user')->oldest(),
-            ]);
+            'requester',
+            'technician',
+            'category',
+            'priority',
+            'ticketStatus',
+            'comments' => fn($query) => $query->with('user')->oldest(),
+        ]);
 
         return view('tickets.show', ['ticket' => $ticket]);
     }
