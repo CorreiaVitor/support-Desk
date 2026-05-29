4217b0dc3a481598b05a17421409ba30d9813f56
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-29 19:54:21 -0300

    refactor: rename comment request to store ticket comment request

--- app/Http/Requests/StoreTicketCommentRequest.php
+++ app/Http/Requests/StoreTicketCommentRequest.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Validation\ValidationRule;
 use Illuminate\Foundation\Http\FormRequest;
 use Override;
 
-class CreateCommentRequest extends FormRequest
+class StoreTicketCommentRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
