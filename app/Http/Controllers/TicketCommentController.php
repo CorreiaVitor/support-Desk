d387134d3551803fd666dc2ff7e5a93048d05434
Vitor Correia <vitorcorreia.git@gmail.com>
2026-05-29 20:02:44 -0300

    refactor: use store ticket comment request

--- app/Http/Controllers/TicketCommentController.php
+++ app/Http/Controllers/TicketCommentController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\CreateCommentRequest;
+use App\Http\Requests\StoreTicketCommentRequest;
 use App\Models\Ticket;
 use App\Models\TicketComment;
 use App\Models\User;
@@ -10,21 +10,20 @@ use Illuminate\Http\RedirectResponse;
 
 class TicketCommentController extends Controller
 {
-    public function store(Ticket $ticket, CreateCommentRequest $request): RedirectResponse
+    public function store(Ticket $ticket, StoreTicketCommentRequest $request): RedirectResponse
     {
         //Estou buscando o id do usuário dessa forma, enquando não utilizo autenticação.
-       $user = User::where('email', 'maria.oliveira@supportdesk.test')->firstOrFail();
+        $user = User::where('email', 'maria.oliveira@supportdesk.test')->firstOrFail();
 
-       TicketComment::create(
-        [
-            'ticket_id' => $ticket->id,
-            'user_id' => $user->id,
-            'body' => $request->validated('comment'),
-            'is_internal' => $request->validated('commentType')
-        ]
-       );
+        TicketComment::create(
+            [
+                'ticket_id' => $ticket->id,
+                'user_id' => $user->id,
+                'body' => $request->validated('comment'),
+                'is_internal' => $request->validated('commentType')
+            ]
+        );
 
-       return to_route('tickets.show', $ticket->number)->with('success', 'Comentário feito com sucesso!');
-
+        return to_route('tickets.show', $ticket->number)->with('success', 'Comentário feito com sucesso!');
     }
 }
