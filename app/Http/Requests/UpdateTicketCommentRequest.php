b56e638a3d5ca1d94f34a0f653e06087476078ad
Vitor Correia <vitorcorreia.git@gmail.com>
2026-06-04 12:24:02 -0300

    feat: add ticket comment update request

---
+++ app/Http/Requests/UpdateTicketCommentRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+use Override;
+
+class UpdateTicketCommentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'commentType' => ['required', 'boolean'],
+            'comment' => ['required', 'string']
+        ];
+    }
+
+    public function messages(): array 
+    {
+        return [
+            'comment.required' => 'Faça um comentário!',
+            'commentType.required' => 'Preencher o campo tipo de comentário!'
+        ];
+    }
+}
